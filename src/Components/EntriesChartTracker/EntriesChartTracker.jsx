40557254da9db91f9145356905f6823ec847e4de
Lucy Ruiz <lucyruiz846@gmail.com>
2022-10-18 15:25:17 -0700

    added chart and setChartData

---
+++ src/Components/EntriesChartTracker/EntriesChartTracker.jsx
@@ -0,0 +1,26 @@
+import { Chart } from "react-google-charts";
+import React, { useState, useEffect } from 'react';
+
+const EntriesChartTracker = (props) => {
+
+    const [chartData, setChartData] = useState([]);
+
+    useEffect(() =>{
+        let tempChartData = props.parentEntries.map(entry => {
+            return [entry.date, entry.weight];
+        });
+        setChartData(tempChartData);
+    })
+
+    return ( 
+        <Chart
+        chartType="LineChart"
+        data={[["Date", "Weight"], ...chartData]}
+        width="100%"
+        height="400px"
+        legendToggle
+        />
+     );
+}
+ 
+export default EntriesChartTracker;
